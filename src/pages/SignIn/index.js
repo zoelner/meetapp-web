061e3f8085b31eb42d7b98e9cc1d8092e8726f5e
Everton Zoelner <everton.zoelner@gmail.com>
2019-08-09 01:05:02 -0300

    Adicionado SignIn

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { ReactComponent as Logo } from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignIn() {
+  function handleSubmit({ email, password }) {
+    console.tron.log({ email, password });
+  }
+
+  return (
+    <>
+      <Logo className="logo" alt="MeetApp" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
