243315fcfedd383e49d7ef36b29e48a0ec0d5934
Everton Zoelner <everton.zoelner@gmail.com>
2019-08-10 01:48:28 -0300

    Adicionado tela dashboard

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+import Button from '~/components/Button';
+
+import { Container, ContentHeader, List, ListItem } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <ContentHeader>
+        <h1>Meus meetups</h1>
+        <Button type="button">Novo Meetup</Button>
+      </ContentHeader>
+
+      <List>
+        <ListItem>
+          <strong>Meetup de React Native</strong>
+          <div>
+            <span>24 de Junho, às 20h</span>
+            <MdKeyboardArrowRight size={24} />
+          </div>
+        </ListItem>
+        <ListItem>
+          <strong>Meetup de React Native</strong>
+          <div>
+            <span>24 de Junho, às 20h</span>
+            <MdKeyboardArrowRight size={24} />
+          </div>
+        </ListItem>
+        <ListItem>
+          <strong>Meetup de React Native</strong>
+          <div>
+            <span>24 de Junho, às 20h</span>
+            <MdKeyboardArrowRight size={24} />
+          </div>
+        </ListItem>
+      </List>
+    </Container>
+  );
+}
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    color: #fff;
+    font-size: 32px;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  margin-top: 48px;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 30px;
+
+  background: rgba(0, 0, 0, 0.1);
+  height: 62px;
+  border-radius: 4px;
+
+  :not(:first-child) {
+    margin-top: 10px;
+  }
+
+  strong {
+    color: #fff;
+    font-size: 18px;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.6);
+
+    span {
+      margin-right: 33px;
+    }
+  }
+`;
