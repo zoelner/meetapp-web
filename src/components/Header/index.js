a6d4dd04ac51d0ea1ab133b59011fd81c16616da
Everton Zoelner <everton.zoelner@gmail.com>
2019-08-12 01:24:05 -0300

    Adicionado funcionalidades ao Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '~/assets/logo.svg';
 
-import { Container, Content, Profile } from './styles';
 import Button from '../Button';
+import { Container, Content, Profile } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -17,10 +26,12 @@ export default function Header() {
         <aside>
           <Profile>
             <div>
-              <strong>Everton Zoelner</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <Button type="submit">Sair</Button>
+            <Button type="submit" onClick={handleSignOut}>
+              Sair
+            </Button>
           </Profile>
         </aside>
       </Content>
